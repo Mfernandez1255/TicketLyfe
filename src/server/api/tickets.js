57effd97cf86c61e3dce0818f90adaa69acde34a
Mfernandez1255 <maurice.fernandez1255@gmail.com>
2023-12-09 12:19:02 -0500

    added GET ticket by id

--- src/server/api/tickets.js
+++ src/server/api/tickets.js
@@ -14,3 +14,16 @@ router.get("/", async (req, res, next) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+
+    const ticket = await prisma.ticket.findUnique({ where: { id } });
+
+    res.json(ticket);
+  } catch (err) {
+    console.error("Error retrieving ticket:", err);
+    next(err);
+  }
+});
