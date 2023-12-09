ae32b12fdb202d34f3eb9c2280cca73d5b664b5d
Mfernandez1255 <maurice.fernandez1255@gmail.com>
2023-12-09 12:38:30 -0500

    added post ticket

--- src/server/api/tickets.js
+++ src/server/api/tickets.js
@@ -27,3 +27,32 @@ router.get("/:id", async (req, res, next) => {
     next(err);
   }
 });
+
+router.post("/", async (req, res, next) => {
+  try {
+    const {
+      eventName,
+      location,
+      dateTime,
+      description,
+      seatSection,
+      imageUrl,
+      price,
+    } = req.body;
+
+    const sellTicket = await prisma.ticket.create({
+      data: {
+        eventName: eventName,
+        location: location,
+        dateTime: dateTime,
+        description: description,
+        seatSection: seatSection,
+        imageUrl: imageUrl,
+        price: price,
+      },
+    });
+    res.json(sellTicket);
+  } catch (err) {
+    next(err);
+  }
+});
