707ab9eac8fb5b8311d5999c5b8a5e2fd20c860e
Mfernandez1255 <maurice.fernandez1255@gmail.com>
2023-12-12 16:32:48 -0500

    styles for the homepage

--- src/client/features/tickets/TicketList.jsx
+++ src/client/features/tickets/TicketList.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useGetTicketsQuery } from "./ticketSlice";
 import TicketCard from "./TicketCard";
 import TicketForm from "./TicketForm";
+import "./Styling/TicketList.less";
+
 function TicketList() {
   const { data: tickets, isLoading, isError } = useGetTicketsQuery();
 
@@ -9,14 +11,16 @@ function TicketList() {
   if (isError || !tickets) return <p>Error fetching tickets.</p>;
 
   return (
-    <div className="ticket-list">
-      <TicketForm />
-      <ul>
+    <>
+      <div className="Ticket_form_header">
+        <TicketForm />
+      </div>
+      <ul className="Ticket-flex">
         {tickets.map((ticket) => (
           <TicketCard key={ticket.id} ticket={ticket} />
         ))}
       </ul>
-    </div>
+    </>
   );
 }
 
